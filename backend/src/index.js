7adad5ee428cc75a73367fe1402b233670689572
Rafael Bidese <rafaelbidese@gmail.com>
2020-01-15 23:05:06 -0600

    Add cors to the backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://rafael:<pwd>@cluster0-z0d10.mongodb.net/database?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://rafael:<password>@cluster0-z0d10.mongodb.net/database?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
+
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
-
 
 app.listen(3333);
 
